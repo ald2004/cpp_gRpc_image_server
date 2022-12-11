cefa85989d712cd5d5d19464133487c68df8fe65
ald2004 <yuanpu@139.com>
2022-12-11 16:51:25 +0800

    init

---
+++ boeRpcAsyncClient.cc
@@ -0,0 +1,186 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <fstream>
+
+#include <opencv2/opencv.hpp>
+
+#include <grpc/support/log.h>
+#include <grpcpp/grpcpp.h>
+
+#include "image.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientAsyncResponseReader;
+using grpc::ClientContext;
+using grpc::CompletionQueue;
+using grpc::Status;
+using boe::upLoadImage;
+using boe::ABImage;
+using boe::UploadReply;
+
+class BoeImageClient {
+ public:
+  explicit BoeImageClient(std::shared_ptr<Channel> channel)
+      : stub_(upLoadImage::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  std::string upLoadImage(const cv::Mat& img) {
+    // Data we are sending to the server.
+    ABImage request;
+    // request.set_name(user);
+
+    std::ostringstream os;
+    os << img.data;                             // Put to the stream
+    std::string asStr = os.str();             // Get string 
+    // asStr.pop_back();                         // Remove brackets
+    // asStr.erase(0,1);
+    request.set_data(asStr);
+    // Container for the data we expect from the server.
+    UploadReply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The producer-consumer queue we use to communicate asynchronously with the
+    // gRPC runtime.
+    CompletionQueue cq;
+
+    // Storage for the status of the RPC upon completion.
+    Status status;
+
+    // stub_->PrepareAsyncSayHello() creates an RPC object, returning
+    // an instance to store in "call" but does not actually start the RPC
+    // Because we are using the asynchronous API, we need to hold on to
+    // the "call" instance in order to get updates on the ongoing RPC.
+    std::unique_ptr<ClientAsyncResponseReader<UploadReply> > rpc(
+        stub_->PrepareAsyncdoUpload(&context, request, &cq));
+
+    // StartCall initiates the RPC call
+    rpc->StartCall();
+
+    // Request that, upon completion of the RPC, "reply" be updated with the
+    // server's response; "status" with the indication of whether the operation
+    // was successful. Tag the request with the integer 1.
+    rpc->Finish(&reply, &status, (void*)1);
+    void* got_tag;
+    bool ok = false;
+    // Block until the next result is available in the completion queue "cq".
+    // The return value of Next should always be checked. This return value
+    // tells us whether there is any kind of event or the cq_ is shutting down.
+    GPR_ASSERT(cq.Next(&got_tag, &ok));
+
+    // Verify that the result from "cq" corresponds, by its tag, our previous
+    // request.
+    GPR_ASSERT(got_tag == (void*)1);
+    // ... and that the request was completed successfully. Note that "ok"
+    // corresponds solely to the request for updates introduced by Finish().
+    GPR_ASSERT(ok);
+
+    // Act upon the status of the actual RPC.
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      return "RPC failed";
+    }
+  }
+  std::string upLoadImage(const std::string& img) {
+    // Data we are sending to the server.
+    ABImage request;
+    request.set_data(img);
+    // Container for the data we expect from the server.
+    UploadReply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The producer-consumer queue we use to communicate asynchronously with the
+    // gRPC runtime.
+    CompletionQueue cq;
+
+    // Storage for the status of the RPC upon completion.
+    Status status;
+
+    // stub_->PrepareAsyncSayHello() creates an RPC object, returning
+    // an instance to store in "call" but does not actually start the RPC
+    // Because we are using the asynchronous API, we need to hold on to
+    // the "call" instance in order to get updates on the ongoing RPC.
+    std::unique_ptr<ClientAsyncResponseReader<UploadReply> > rpc(
+        stub_->PrepareAsyncdoUpload(&context, request, &cq));
+
+    // StartCall initiates the RPC call
+    rpc->StartCall();
+
+    // Request that, upon completion of the RPC, "reply" be updated with the
+    // server's response; "status" with the indication of whether the operation
+    // was successful. Tag the request with the integer 1.
+    rpc->Finish(&reply, &status, (void*)1);
+    void* got_tag;
+    bool ok = false;
+    // Block until the next result is available in the completion queue "cq".
+    // The return value of Next should always be checked. This return value
+    // tells us whether there is any kind of event or the cq_ is shutting down.
+    GPR_ASSERT(cq.Next(&got_tag, &ok));
+
+    // Verify that the result from "cq" corresponds, by its tag, our previous
+    // request.
+    GPR_ASSERT(got_tag == (void*)1);
+    // ... and that the request was completed successfully. Note that "ok"
+    // corresponds solely to the request for updates introduced by Finish().
+    GPR_ASSERT(ok);
+
+    // Act upon the status of the actual RPC.
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      return "RPC failed";
+    }
+  }
+ private:
+  // Out of the passed in Channel comes the stub, stored here, our view of the
+  // server's exposed services.
+  std::unique_ptr<upLoadImage::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint (in this case,
+  // localhost at port 50051). We indicate that the channel isn't authenticated
+  // (use of InsecureChannelCredentials()).
+  BoeImageClient client(grpc::CreateChannel(
+      "localhost:50051", grpc::InsecureChannelCredentials()));
+//   std::string user("world");
+//   auto img=cv::imread("/dev/shm/xxx.jpg");
+//   std::string reply = client.upLoadImage(img);  // The actual RPC call!
+//   std::cout << "client received: " << reply << std::endl;
+
+  std::ifstream fs("/dev/shm/xxx.jpg",std::ifstream::binary);
+  if(fs.good()){
+    std::ostringstream os;
+    os << fs.rdbuf();
+    std::string reply = client.upLoadImage(os.str());
+    std::cout << "client received: " << reply << std::endl;
+  }
+  return 0;
+}
