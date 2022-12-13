93b9a06f5937e07d2db2b50f4c4a9763cf04ce60
ald2004 <yuanpu@139.com>
2022-12-13 15:28:45 +0800

    Update boeRpcAsyncClient.cc

--- boeRpcAsyncClient.cc
+++ boeRpcAsyncClient.cc
@@ -19,12 +19,14 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 #include <fstream>
-
-#include <opencv2/opencv.hpp>
+#include <filesystem>
+#include <chrono>
 
 #include <grpc/support/log.h>
 #include <grpcpp/grpcpp.h>
+
 
 #include "image.grpc.pb.h"
 
@@ -34,133 +36,99 @@ using grpc::ClientContext;
 using grpc::CompletionQueue;
 using grpc::Status;
 using boe::upLoadImage;
-using boe::ABImage;
 using boe::UploadReply;
+using boe::ABImage;
 
 class BoeImageClient {
  public:
   explicit BoeImageClient(std::shared_ptr<Channel> channel)
       : stub_(upLoadImage::NewStub(channel)) {}
 
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-  std::string upLoadImage(const cv::Mat& img) {
+  // Assembles the client's payload and sends it to the server.
+  void upLoadImage(const std::string& user) {
     // Data we are sending to the server.
     ABImage request;
-    // request.set_name(user);
 
-    std::ostringstream os;
-    os << img.data;                             // Put to the stream
-    std::string asStr = os.str();             // Get string 
-    // asStr.pop_back();                         // Remove brackets
-    // asStr.erase(0,1);
-    request.set_data(asStr);
-    // Container for the data we expect from the server.
-    UploadReply reply;
+    // std::cout << "get data : " << std::string(user).substr(0,10) << "and " << size<< std::endl;
+    request.set_data(user);
+    // request.set_data(rowData);
+    // request.set_data(user);
+    // request.set_data("set_message");
+    // request.set_msg("set_message");
+    // request.set_width(300);request.set_height(300);
 
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-
-    // The producer-consumer queue we use to communicate asynchronously with the
-    // gRPC runtime.
-    CompletionQueue cq;
-
-    // Storage for the status of the RPC upon completion.
-    Status status;
+    // Call object to store rpc data
+    AsyncClientCall* call = new AsyncClientCall;
 
     // stub_->PrepareAsyncSayHello() creates an RPC object, returning
     // an instance to store in "call" but does not actually start the RPC
     // Because we are using the asynchronous API, we need to hold on to
     // the "call" instance in order to get updates on the ongoing RPC.
-    std::unique_ptr<ClientAsyncResponseReader<UploadReply> > rpc(
-        stub_->PrepareAsyncdoUpload(&context, request, &cq));
+    // call->response_reader =
+    //     stub_->PrepareAsyncSayHello(&call->context, request, &cq_);
+    call->response_reader = 
+        stub_->PrepareAsyncdoUpload(&call->context,request,&cq_);
 
     // StartCall initiates the RPC call
-    rpc->StartCall();
+    call->response_reader->StartCall();
 
     // Request that, upon completion of the RPC, "reply" be updated with the
     // server's response; "status" with the indication of whether the operation
-    // was successful. Tag the request with the integer 1.
-    rpc->Finish(&reply, &status, (void*)1);
+    // was successful. Tag the request with the memory address of the call
+    // object.
+    call->response_reader->Finish(&call->reply, &call->status, (void*)call);
+  }
+
+  // Loop while listening for completed responses.
+  // Prints out the response from the server.
+  void AsyncCompleteRpc() {
     void* got_tag;
     bool ok = false;
+
     // Block until the next result is available in the completion queue "cq".
-    // The return value of Next should always be checked. This return value
-    // tells us whether there is any kind of event or the cq_ is shutting down.
-    GPR_ASSERT(cq.Next(&got_tag, &ok));
+    while (cq_.Next(&got_tag, &ok)) {
+      // The tag in this example is the memory location of the call object
+      AsyncClientCall* call = static_cast<AsyncClientCall*>(got_tag);
 
-    // Verify that the result from "cq" corresponds, by its tag, our previous
-    // request.
-    GPR_ASSERT(got_tag == (void*)1);
-    // ... and that the request was completed successfully. Note that "ok"
-    // corresponds solely to the request for updates introduced by Finish().
-    GPR_ASSERT(ok);
+      // Verify that the request was completed successfully. Note that "ok"
+      // corresponds solely to the request for updates introduced by Finish().
+      GPR_ASSERT(ok);
 
-    // Act upon the status of the actual RPC.
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      return "RPC failed";
+      if (call->status.ok())
+        std::cout<<" .";
+        // std::cout << "Greeter received: "<<call->status.ok() << " " << call->reply.message() << std::endl;
+      else
+        std::cout << "RPC failed: "<<call->status.ok() << " "<< call->reply.message()  << std::endl;
+        
+
+      // Once we're complete, deallocate the call object.
+      delete call;
     }
   }
-  std::string upLoadImage(const std::string& img) {
-    // Data we are sending to the server.
-    ABImage request;
-    request.set_data(img);
-    // Container for the data we expect from the server.
-    UploadReply reply;
 
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-
-    // The producer-consumer queue we use to communicate asynchronously with the
-    // gRPC runtime.
-    CompletionQueue cq;
-
-    // Storage for the status of the RPC upon completion.
-    Status status;
-
-    // stub_->PrepareAsyncSayHello() creates an RPC object, returning
-    // an instance to store in "call" but does not actually start the RPC
-    // Because we are using the asynchronous API, we need to hold on to
-    // the "call" instance in order to get updates on the ongoing RPC.
-    std::unique_ptr<ClientAsyncResponseReader<UploadReply> > rpc(
-        stub_->PrepareAsyncdoUpload(&context, request, &cq));
-
-    // StartCall initiates the RPC call
-    rpc->StartCall();
-
-    // Request that, upon completion of the RPC, "reply" be updated with the
-    // server's response; "status" with the indication of whether the operation
-    // was successful. Tag the request with the integer 1.
-    rpc->Finish(&reply, &status, (void*)1);
-    void* got_tag;
-    bool ok = false;
-    // Block until the next result is available in the completion queue "cq".
-    // The return value of Next should always be checked. This return value
-    // tells us whether there is any kind of event or the cq_ is shutting down.
-    GPR_ASSERT(cq.Next(&got_tag, &ok));
-
-    // Verify that the result from "cq" corresponds, by its tag, our previous
-    // request.
-    GPR_ASSERT(got_tag == (void*)1);
-    // ... and that the request was completed successfully. Note that "ok"
-    // corresponds solely to the request for updates introduced by Finish().
-    GPR_ASSERT(ok);
-
-    // Act upon the status of the actual RPC.
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      return "RPC failed";
-    }
-  }
  private:
+  // struct for keeping state and data information
+  struct AsyncClientCall {
+    // Container for the data we expect from the server.
+    UploadReply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // Storage for the status of the RPC upon completion.
+    Status status;
+
+    std::unique_ptr<ClientAsyncResponseReader<UploadReply>> response_reader;
+  };
+
   // Out of the passed in Channel comes the stub, stored here, our view of the
   // server's exposed services.
   std::unique_ptr<upLoadImage::Stub> stub_;
+
+  // The producer-consumer queue we use to communicate asynchronously with the
+  // gRPC runtime.
+  CompletionQueue cq_;
 };
 
 int main(int argc, char** argv) {
@@ -169,18 +137,47 @@ int main(int argc, char** argv) {
   // localhost at port 50051). We indicate that the channel isn't authenticated
   // (use of InsecureChannelCredentials()).
   BoeImageClient client(grpc::CreateChannel(
-      "localhost:50051", grpc::InsecureChannelCredentials()));
-//   std::string user("world");
-//   auto img=cv::imread("/dev/shm/xxx.jpg");
-//   std::string reply = client.upLoadImage(img);  // The actual RPC call!
-//   std::cout << "client received: " << reply << std::endl;
+      "localhost:50052", grpc::InsecureChannelCredentials()));
 
-  std::ifstream fs("../xx.jpg",std::ifstream::binary);
+  // Spawn reader thread that loops indefinitely
+  std::thread thread_ = std::thread(&BoeImageClient::AsyncCompleteRpc, &client);
+  // std::string path("/aiplatform/lifei/detect/dataset_car_train/reault_vehicle/train/186315.jpg");
+  std::string path("../xxx.jpg");
+  
+  std::ifstream fs(path,std::ifstream::in | std::ios::binary);
+  std::ostringstream os;
+  std::string user;  
   if(fs.good()){
-    std::ostringstream os;
     os << fs.rdbuf();
-    std::string reply = client.upLoadImage(os.str());
-    std::cout << "client received: " << reply << std::endl;
+    user=os.str();
+  }else{
+    std::cout << "------------------------------" << std::endl;
   }
+
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+  
+  for (int i = 0; i < atoi(argv[1]); i++) {
+    // std::string user("world " + std::to_string(i));
+    // client.upLoadImage(user);  // The actual RPC call!
+    
+    
+    
+    //   // std::string reply = client.upLoadImage(os.str());
+    //   std::cout<< os.str().substr(0,20) <<std::endl;
+      // std::cout << "ccccccccc "<< user.substr(0,8) <<std::endl;
+      // std::cout << "sent data : " << user.substr(0,10) << "and " <<user.length()<< std::endl;
+      client.upLoadImage(user);
+      // std::cout<< user.max_size() <<std::endl;
+    // }
+  }
+
+
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  std::cout << std::endl<<std::flush;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds> (end - begin).count() << "[us]" << std::endl<<std::flush;
+  std::cout << "Press control-c to quit" << std::endl << std::endl;
+  thread_.join();  // blocks forever
+
   return 0;
 }
